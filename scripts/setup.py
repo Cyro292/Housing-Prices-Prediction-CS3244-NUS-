a1131afd9c59839fa491da648b74273bd70cfe00
Cyro292 <amon@archi.de>
2025-04-08 14:17:31 +0800

    ladlkkalsdfkajsdflaskdfjalsdfjla

---
+++ scripts/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cs3244-assignment1',
+    version='0.1.0',
+    description='CS3244 Assignment 1 - Housing Price Prediction',
+    author='Amon Sisowath',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
